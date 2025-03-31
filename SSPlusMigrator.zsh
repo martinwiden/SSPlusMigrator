38f1d4655f3c3c3e453c1a141e02fa000046cdc6
Martin Widén <martinjwiden@gmail.com>
2025-03-31 19:18:36 +0200

    Added function to check if Self Service Classic is running
    If Self Service Classic is running, send a notification to the user using swiftdialog telling the user to quit the app and try again

--- SSPlusMigrator.zsh
+++ SSPlusMigrator.zsh
@@ -1,10 +1,23 @@
 #!/bin/zsh
 
-# Made by Martin Widén at Linköping University 2025.
-# This script uninstalls Self Service and installs Self Service+.
-# It is designed to be run on macOS devices managed by Jamf Pro.
-# It removes the existing Self Service application and its associated files,
-# then triggers a Jamf Pro policy to install Self Service+.
+# Author: Martin Widén, Linköping University, 2025
+# This script starts a migration from Self Service Classic to
+# Self Service+ on macOS devices managed by Jamf Pro.
+# It ensures that Self Service Classic is not running,
+# uninstalls the application along with its associated files,
+# and triggers a Jamf Pro policy to install Self Service+.
+
+check_self_service_running() {
+    if pgrep -xq "Self Service"; then
+        /Library/Application\ Support/Dialog/Dialog.app/Contents/MacOS/Dialog \
+            --notification \
+            --title "Migration Notice" \
+            --message "Please quit Self Service before proceeding with the migration." \
+            --icon "/Applications/Self Service.app/Contents/Resources/AppIcon.icns" \
+            --overlayicon caution \
+            exit 1
+    fi
+}
 
 uninstall_self_service() {
     # Paths to remove
@@ -35,7 +48,7 @@ uninstall_self_service() {
 
 install_self_service_plus() {
     # Trigger Jamf Pro policy to install Self Service+
-    # Edit this to the name of your custom trigger for the policy 
+    # Edit this to the name of your custom trigger for the policy
     /usr/local/bin/jamf policy -event installSelfServicePlus
     if [ $? -eq 0 ]; then
         print "Triggered Jamf Pro policy to install Self Service+."
@@ -44,6 +57,8 @@ install_self_service_plus() {
     fi
 }
 
+# Main script execution
+check_self_service_running
 print "Uninstalling Self Service..."
 uninstall_self_service
 print "Installing Self Service+..."
